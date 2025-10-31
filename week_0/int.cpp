bddd102dea4f6d7fe6c83ce4994d4ed77661e12f
Konit611 <konit611@gmail.com>
2025-10-31 12:37:02 +0900

    Cpp Basic Grammar _ Integer

---
+++ week_0/int.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 13;
+  int b = 4;
+  
+  cout << "a + b : " << a + b << endl;
+  cout << "a - b : " << a - b << endl;
+  cout << "a * b : " << a * b << endl;
+  cout << "a / b : " << a / b << endl;
+  cout << "a % b : " << a % b << endl;
+  cout << "-a : " << -a << endl;
+
+  cout << "a == b : " << (a == b) << endl;
+  cout << "a != b : " << (a != b) << endl;
+  cout << "a > b : " << (a > b) << endl;
+  cout << "a < b : " << (a < b) << endl;
+  cout << "a >= b : " << (a >= b) << endl;
+  cout << "a <= b : " << (a <= b) << endl; 
+
+  cout << "a & b : " << (a & b) << endl;
+  cout << "a | b : " << (a | b) << endl;
+
+  return 0;
+}
